991b97a553e1caa49f0dae1628426e0133b600bc
jangjeonghun1004 <jangjeonghun1004@gmail.com>
2025-01-09 18:21:19 +0900

    init

--- Dockerfile
+++ Dockerfile
@@ -5,3 +5,11 @@ ARG ENV
 COPY ${JAR_FILE} app.jar
 ENTRYPOINT ["java", "-Dspring.profiles.active=${PROFILES}", "-Dserver.env=${ENV}", "-jar", "app.jar"]
 
+
+
+mkdir -p /my/local/data
+sudo chown -R 1001:1001 /my/local/data
+
+docker run --name my-mysql -e MYSQL_ROOT_PASSWORD=root -p 3306:3306 -v /my/local/data:/var/lib/mysql -d mysql
+
+
--- src/main/java/com/example/demo/repository/ToDoRepository.java
+++ src/main/java/com/example/demo/repository/ToDoRepository.java
@@ -1,11 +1,7 @@
 package com.example.demo.repository;
 
 import com.example.demo.entity.ToDoEntity;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface ToDoRepository extends JpaRepository<ToDoEntity, Long> {
-//}
+import org.springframework.data.jpa.repository.JpaRepository;
 
-
-public interface ToDoRepository {
+public interface ToDoRepository extends JpaRepository<ToDoEntity, Long> {
 }
